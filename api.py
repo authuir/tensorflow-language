eccfc3df9c8c67ef7698921e0be5b8cfd6768d0a
DESKTOP-N8NE5NS\Authuir <authuir@authuir.com>
2017-03-23 18:51:09 +0800

    no message

--- api.py
+++ api.py
@@ -4,6 +4,7 @@
 import web
 import json
 import subprocess
+from vietnamese_CNN import predic
 
 urls = (
     '/api', 'Api',
@@ -14,7 +15,7 @@ class Api:
     def POST(self):  
         i = web.input().data
         data = json.loads(i)
-        return data[0]
+        return predic(data)
 
 if __name__ == "__main__":
     web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
