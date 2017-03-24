e10971a5654a7e6c3df0f4add524c3187c6906f0
DESKTOP-N8NE5NS\Authuir <authuir@authuir.com>
2017-03-24 17:52:18 +0800

    Added Softmax method

--- api.py
+++ api.py
@@ -2,10 +2,7 @@
 #coding=utf-8
 
 import web
-import json
 import subprocess
-from vietnamese_CNN import predic
-import vietnamese
 
 urls = (
     '/api', 'Api',
@@ -15,10 +12,13 @@ app = web.application(urls,globals())
 class Api:
     def POST(self):  
         i = web.input().data
-        data = json.loads(i)
-        rate, code = predic(data)
-        rate1, code1 = vietnamese.predic(data)
-        rtn = '["%f","%f","%f","%s","%s","%s","%f","%f","%f","%s","%s","%s"]' % ( rate1[0],rate1[1],rate1[2],code1[0],code1[1],code1[2],rate[0],rate[1],rate[2],code[0],code[1],code[2])
+        process = subprocess.Popen(['python','./vietnamese.py',i],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+        out_text = process.stdout.read().decode('utf-8')
+        rtn = ""
+        rtn = rtn+out_text.split('\r')[0]
+        process = subprocess.Popen(['python','./vietnamese_CNN.py',i],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+        out_text = process.stdout.read().decode('utf-8')
+        rtn += out_text.split('\r')[0]
         return rtn
 
 if __name__ == "__main__":
